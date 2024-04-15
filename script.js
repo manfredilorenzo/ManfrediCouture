5a246d1e3d0b846ed7c5b5c0a975914f60608304
Lorenzo Manfredi <manfredilorenzo@itis-molinari.eu>
2024-04-15 21:34:42 +0200

    cambio sfondo, fine modale, prova js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    console.log("js funziona");
   }
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    
   }
